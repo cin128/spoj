973f2884722f7c707efcd1c9ffdefb9bec1d9648
mjl <mj@mj.com>
2018-09-09 18:52:39 +0200

    609

---
+++ src/spoj/id609/ETI06F1/Main.java
@@ -0,0 +1,16 @@
+package spoj.id609.ETI06F1;
+
+import java.util.Scanner;
+
+public class Main {
+	private static double PI = 3.141592654;
+	
+	public static void main(String[] args) {
+		Scanner s = new Scanner(System.in);
+		double r = s.nextFloat();
+		double d = s.nextFloat();		
+		double result=PI*(r*r-d*d/4);
+		System.out.println(result);
+		s.close();		
+	}
+}
